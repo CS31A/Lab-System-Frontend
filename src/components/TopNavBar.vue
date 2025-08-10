d373d55c58d5691d5e82b232f39626e589453d22
Felaeia <elizabeth2ndcando@gmail.com>
2025-08-10 18:16:23 +0800

    feat: Add component 'LabAvailabilty' & 'TopNavBar'
    -Created a new component for top navigations and Lav Availability for viewing of available labs.

--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -1,55 +1,45 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
+import { RouterLink } from 'vue-router'
 
 defineOptions({
   name: 'TopBar',
 })
-
-const route = useRoute()
-const router = useRouter()
-
-// Created so that the title can be dynamic based on the route
-const routeTitles: Record<string, string> = {
-  dashboard: 'ACLC Dashboard',
-  labs: 'ACLC Lab Management',
-  students: 'ACLC Students',
-  settings: 'ACLC Settings',
-}
-
-const currentTitle = ref(routeTitles[route.name as string] || 'ACLC')
-
-watch(
-  () => route.name,
-  (newName) => {
-    currentTitle.value = routeTitles[newName as string] || 'ACLC'
-  },
-)
-
-function goTo(path: string) {
-  router.push({ name: path })
-}
 </script>
 
 <template>
   <nav class="bg-blue-600 text-white px-6 py-3 flex items-center justify-between shadow-md">
     <div class="font-semibold text-lg">
-      {{ currentTitle }}
+      <p>
+        Placeholder for Top Navigation Bar
+      </p>
     </div>
 
     <div class="flex items-center space-x-6">
-      <button class="hover:underline" @click="goTo('')">
-        Dashboard
-      </button>
-      <button class="hover:underline" @click="goTo('/LabAvailability')">
-        Labs
-      </button>
-      <button class="hover:underline" @click="goTo('')">
-        Students
-      </button>
-      <button class="hover:underline" @click="goTo('')">
-        Settings
-      </button>
+      <!-- Path Doesnt Exist -->
+      <p class="hover:underline">
+        <RouterLink to="">
+          Dashboard
+        </RouterLink>
+      </p>
+
+      <p class="hover:underline">
+        <RouterLink to="/lab_availability">
+          Lab Availability
+        </RouterLink>
+      </p>
+      <!-- Path Doesnt Exist -->
+      <p class="hover:underline">
+        <RouterLink to="/students">
+          Students
+        </RouterLink>
+      </p>
+
+      <!-- Path Doesnt Exist -->
+      <p class="hover:underline">
+        <RouterLink to="/settings">
+          Settings
+        </RouterLink>
+      </p>
 
       <div class="w-20 h-8 rounded-full flex items-center justify-center">
         <img src="/images/logo.png" alt="ACLC Logo" class="w-20 h-20 p-3 rounded-full">
